0763d5d5d7a0b3a7ec4c30bdbd8f29f24d22daf0
Efren Almanza Lamas <37811579+SpyroCode@users.noreply.github.com>
2020-06-24 22:35:41 -0500

    maquetado punto pixel

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -11,12 +11,12 @@ session_start();
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-  <title>Inventory System</title>
+  <title>PuntoPixel</title>
 
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-  <link rel="icon" href="vistas/img/plantilla/icono-negro.png">
+  <link rel="icon" href="vistas/img/plantilla/logo_manchas.png">
 
    <!--=====================================
   PLUGINS DE CSS
